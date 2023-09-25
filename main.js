72fb60309423c3754355b43c7527517d3c51c5c9
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2023-09-25 07:27:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-let cot=document.getElementById("cont")
-cot.style.color="blue"
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz8Zx1W5LAzBxmhvpnqMO6SruPcuW2NAAlLcxjMB28KuUTAte5l_93r6Jb8gj2qsHy-/exec'
+const form = document.forms['google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("Thanks for Contacting me..! Your message has been received..."))
+        .catch(error => console.error('Error!', error.message))
+})e"
